3af064afb7993585331370ea30ad3413e043b6cd
kaifa <yp22322bug@163.com>
2022-12-09 16:11:47 +0800

    api init finish

--- webshopapi/webshopapi/webshopapi/settings/dev.py
+++ webshopapi/webshopapi/webshopapi/settings/dev.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.insert(0, str( BASE_DIR / "apps"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-4u^dh^v1py^q_wuj@dm@*^822embrj-_oro14@zvqj*+$wf1zy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webshopapi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -89,7 +89,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -108,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -122,7 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -133,14 +130,13 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # logging
 # 日志
 LOGGING = {
-    'version': 1, # 使用的日志模块的版本，目前官方提供的只有版本1，但是官方有可能会升级，为了避免升级出现的版本问题，所以这里固定为1
-    'disable_existing_loggers': False, # 是否禁用其他的已经存在的日志功能？肯定不能，有可能有些第三方模块在调用，所以禁用了以后，第三方模块无法捕获自身出现的异常了。
-    'formatters': { # 日志格式设置，verbose或者simple都是自定义的
-        'verbose': { # 详细格式，适合用于开发人员不在场的情况下的日志记录。
+    'version': 1,  # 使用的日志模块的版本，目前官方提供的只有版本1，但是官方有可能会升级，为了避免升级出现的版本问题，所以这里固定为1
+    'disable_existing_loggers': False,  # 是否禁用其他的已经存在的日志功能？肯定不能，有可能有些第三方模块在调用，所以禁用了以后，第三方模块无法捕获自身出现的异常了。
+    'formatters': {  # 日志格式设置，verbose或者simple都是自定义的
+        'verbose': {  # 详细格式，适合用于开发人员不在场的情况下的日志记录。
             # 格式定义：https://docs.python.org/3/library/logging.html#logrecord-attributes
             # levelname 日志等级
             # asctime   发生时间
@@ -149,9 +145,9 @@ LOGGING = {
             # thread    线程ID
             # message   异常信息
             'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{', # 变量格式分隔符
+            'style': '{',  # 变量格式分隔符
         },
-        'simple': { # 简单格式，适合用于开发人员在场的情况下的终端输出
+        'simple': {  # 简单格式，适合用于开发人员在场的情况下的终端输出
             'format': '{levelname} {message}',
             'style': '{',
         },
@@ -161,12 +157,12 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugTrue',
         },
     },
-    'handlers': { # 日志处理流程，console或者mail_admins都是自定义的。
+    'handlers': {  # 日志处理流程，console或者mail_admins都是自定义的。
         'console': {
-            'level': 'DEBUG', # 设置当前日志处理流程中的日志最低等级
-            'filters': ['require_debug_true'], # 当前日志处理流程的日志过滤
+            'level': 'DEBUG',  # 设置当前日志处理流程中的日志最低等级
+            'filters': ['require_debug_true'],  # 当前日志处理流程的日志过滤
             'class': 'logging.StreamHandler',  # 当前日志处理流程的核心类，StreamHandler可以帮我们把日志信息输出到终端下
-            'formatter': 'simple'              # 当前日志处理流程的日志格式
+            'formatter': 'simple'  # 当前日志处理流程的日志格式
         },
         # 'mail_admins': {
         #     'level': 'ERROR',                  # 设置当前日志处理流程中的日志最低等级
@@ -188,15 +184,49 @@ LOGGING = {
     },
     'loggers': {  # 日志处理的命名空间
         'django': {
-            'handlers': ['console','file'], # 当基于django命名空间写入日志时，调用那几个日志处理流程
-            'propagate': True,   # 是否在django命名空间对应的日志处理流程结束以后，冒泡通知其他的日志功能。True表示允许
+            'handlers': ['console', 'file'],  # 当基于django命名空间写入日志时，调用那几个日志处理流程
+            'propagate': True,  # 是否在django命名空间对应的日志处理流程结束以后，冒泡通知其他的日志功能。True表示允许
         },
     }
 }
-
 
 # DRF Setting
 REST_FRAMEWORK = {
     # custom exception
     'EXCEPTION_HANDLER': 'webshopapi.utils.exceptions.custom_exception_handler',
 }
+
+# Redis Config
+# https://django-redis-chs.readthedocs.io/zh_CN/latest/#
+# 设置redis缓存
+CACHES = {
+    # 默认缓存
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        # 项目上线时,需要调整这里的路径
+        "LOCATION": "redis://:123456@127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    # 提供给admin站点的session存储
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://:123456@127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    # 提供存储短信验证码
+    "sms_code":{
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://:123456@127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+# 设置用户登录admin站点时,记录登录状态的session保存到redis缓存中
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# 设置session保存的位置对应的缓存配置项
+SESSION_CACHE_ALIAS = "session"
