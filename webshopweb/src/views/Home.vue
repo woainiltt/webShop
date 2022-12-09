ad78ff267f11b566e8ace063411a79f8fa97049c
kaifa <yp22322bug@163.com>
2022-12-09 18:03:31 +0800

    web config axios

--- webshopweb/src/views/Home.vue
+++ webshopweb/src/views/Home.vue
@@ -7,14 +7,10 @@
 </template>
 
 <script setup>
-import Header from "../views/components/Header.vue"
-import Footer from "../views/components/Footer.vue"
-// import axios from "axios"
-// const http = axios.create()
-// // 配置vue跨域测试服务端的跨域是否配置成功
-// http.get("/api/home/demo/").then(response=>{
-//   console.log(response.data);
-// })
+import Header from "../views/components/Header.vue";
+import Footer from "../views/components/Footer.vue";
+import http from "../utils/http";
+
 </script>
 
 <style scoped>
